cbb28fe83e59d085c16041078565517ee9f79bcb
notInWonderland <75802115+podvalniypiero@users.noreply.github.com>
2023-02-20 20:13:05 +0300

    header fix

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ function App() {
   return (
     <div className="App">
 
-    <div className='wrapper'>
+      <header>
       <div className='headerTop'>
       <p>ЛЕКТОРИЙ ПРОВОДНИК В МИР ИСКУССТВА СЛОВА</p>
       </div>
 
        <div className='headerMain'>
-        <img width={60} src='\img\logo.jpg' alt='logo'></img>
+        <img className='logo' width={60} src='\img\logo.jpg' alt='logo'></img>
         <button>НАША КОЛЛЕКЦИЯ</button>
-        <img width={50} src='\img\burger.jpg' alt='menuBurger'></img>
+        <img className='burger' width={50} src='\img\burger.jpg' alt='menuBurger'></img>
        </div>
-    </div>
+       </header>
     </div>
   );
 }
