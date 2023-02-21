b5839544ee3fba922aa5f5266bb97e1935eee262
notInWonderland <75802115+podvalniypiero@users.noreply.github.com>
2023-02-21 20:06:29 +0300

    footer component

--- src/App.js
+++ src/App.js
@@ -1,52 +1,21 @@
 import './App.scss';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {React, useState} from 'react';
+import Header from './components/Header/Header';
 
 function App() {
-
-  const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
-  const [menu_class, setMenuClass] = useState("menu hidden");
-  const [isMenuClicked, setIsMenuClicked] = useState(false);
-
-
-  const updateMenu = () => {
-      if(!isMenuClicked) {
-          setBurgerClass("burger-bar clicked")
-          setMenuClass("menu visible")
-      }
-      else {
-          setBurgerClass("burger-bar unclicked")
-          setMenuClass("menu hidden")
-      }
-      setIsMenuClicked(!isMenuClicked)
-  }
 
 
   return (
     <div className="App">
+      <Routes>
 
-      <header>
-      <div className='headerTop'>
-      <p>ЛЕКТОРИЙ ПРОВОДНИК В МИР ИСКУССТВА СЛОВА</p>
-      </div>
+        <Route path='/' element = {
+          
+          <Header/>
+        }/>
 
-       <div className='headerMain'>
-        <img className='logo' width={60} src='\img\logo.jpg' alt='logo'></img>
-        <button>НАША КОЛЛЕКЦИЯ</button>
-
-        {/* <img className='burger' width={50} src='\img\burger.jpg' alt='menuBurger'></img> */}
-        <nav>
-                <div className="burger-menu" onClick={updateMenu}>
-                    <div className={burger_class} ></div>
-                    <div className={burger_class} ></div>
-                    <div className={burger_class} ></div>
-                </div>
-            </nav>
-
-            <div className={menu_class}>ИЗБРАННОЕ</div>
-       
-       </div>
-       </header>
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,52 @@
-import styles from './Header.scss';
+import styles from './Header.module.scss';
 import { Route, Link } from 'react-router-dom';
+import {React, useState} from 'react';
 
 function Header(){
-    return(
+    
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
+  const [menu_class, setMenuClass] = useState("menu hidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
 
+
+  const updateMenu = () => {
+      if(!isMenuClicked) {
+          setBurgerClass("burger-bar clicked")
+          setMenuClass("menu visible")
+      }
+      else {
+          setBurgerClass("burger-bar unclicked")
+          setMenuClass("menu hidden")
+      }
+      setIsMenuClicked(!isMenuClicked)
+  }
+
+    return(
+        
+      <header>
+      <div className={styles.headerTop}>
+      <p>ЛЕКТОРИЙ ПРОВОДНИК В МИР ИСКУССТВА СЛОВА</p>
+      </div>
+
+       <div className={styles.headerMain}>
+        <img className={styles.logo} width={60} src='\img\logo.jpg' alt='logo'></img>
+        <button className={styles.btn}>НАША КОЛЛЕКЦИЯ</button>
+
+        {/* <img className='burger' width={50} src='\img\burger.jpg' alt='menuBurger'></img> */}
+        <nav>
+                <div className={styles[`burger-menu`]} onClick={updateMenu}>
+                    <div className={styles.burger_class} ></div>
+                    <div className={styles.burger_class} ></div>
+                    <div className={styles.burger_class} ></div>
+                </div>
+            </nav>
+
+            <div className={styles.menu_class}>ИЗБРАННОЕ</div>
+       
+       </div>
+       </header>
      )
   }
   
-  export default Footer;
+  export default Header;
   
