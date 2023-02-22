7382d5034a0387bbd5830d113cde0437de190702
notInWonderland <75802115+podvalniypiero@users.noreply.github.com>
2023-02-22 15:17:32 +0300

    header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,46 +4,20 @@ import {React, useState} from 'react';
 
 function Header() {
     
-  const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
-  const [menu_class, setMenuClass] = useState("menu hidden");
-  const [isMenuClicked, setIsMenuClicked] = useState(false);
-
-
-  const updateMenu = () => {
-      if(!isMenuClicked) {
-          setBurgerClass("burger-bar clicked")
-          setMenuClass("menu visible")
-      }
-      else {
-          setBurgerClass("burger-bar unclicked")
-          setMenuClass("menu hidden")
-      }
-      setIsMenuClicked(!isMenuClicked)
-  }
-
     return(
         
       <header>
       <div className={styles.headerTop}>
-      <p>ЛЕКТОРИЙ ПРОВОДНИК В МИР ИСКУССТВА СЛОВА</p>
+        <p className={styles.keyframes}>ЛЕКТОРИЙ ПРОВОДНИК В МИР ИСКУССТВА СЛОВА</p>
       </div>
-
        <div className={styles.headerMain}>
-        <img className={styles.logo} width={60} src='\img\logo.jpg' alt='logo'></img>
-        
-        <button className={styles.btn}>НАША КОЛЛЕКЦИЯ</button>
-
-        {/* <img className='burger' width={50} src='\img\burger.jpg' alt='menuBurger'></img> */}
+        <img className={styles.logo} width={60} src='\img\logotype.jpg' alt='logo'></img>
         <nav>
-                <div className={styles[`burger-menu`]} onClick={updateMenu}>
-                    <div className={burger_class} ></div>
-                    <div className={burger_class} ></div>
-                    <div className={burger_class} ></div>
-                </div>
-            </nav>
-
-            <div className={menu_class}></div>
-       
+                 <button>О НАС</button>
+                 <button>НАША КОЛЛЕКЦИЯ</button>
+                 <button>РАЗДЕЛ ИНТЕРАКТИВА</button>
+                 <button>ИЗБРАННОЕ</button>
+        </nav>
        </div>
        </header>
      );
