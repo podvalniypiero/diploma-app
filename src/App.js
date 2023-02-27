aa075ea444169e00731ee5c138715988aee109e7
notInWonderland <75802115+podvalniypiero@users.noreply.github.com>
2023-02-27 22:26:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {React, useState} from 'react';
-import Header from './components/Header/Header';
+import Container from './components/Container/Container';
 
 function App() {
 
@@ -12,7 +12,7 @@ function App() {
 
         <Route path='/' element = {
           
-          <Header/>
+          <Container/>
         }/>
 
       </Routes>
